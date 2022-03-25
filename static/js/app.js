7f149e469c1685329a92c302f86bd4992a132469
Eddy Salgado <edsalgado028@gmail.com>
2022-03-25 16:30:40 -0700

    initial files

---
+++ static/js/app.js
@@ -0,0 +1,71 @@
+
+
+// initialize dashboard
+function initialize()
+{
+    // use d3 to read data
+    d3.json("samples.json").then((data) => {
+        let sampleNames = data.names;
+
+        sampleNames.forEach(element => {
+            select.append("option").text(element).property("value", element);
+        });
+    console.log(data);
+
+    // call function to build metadata
+        demInfo(sampleNames[0]);
+
+        barChart(sampleNames[0]);
+    })
+    
+    var select = d3.select("#selDataset");  
+}
+
+initialize();
+
+// update dashboard
+function optionChanged(item)
+{
+    d3.select("#sample-metadata").html("")
+    demInfo(item);
+
+    barChart(item);
+}
+
+// populates meta data
+function demInfo(sample)
+{
+    d3.json("samples.json").then((data) => {
+        let result = data.metadata.filter(sampleResult => sampleResult.id == sample)[0];
+
+       Object.entries(result).forEach(([key, value]) => {
+        d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+        }) 
+    })
+}
+
+// build graphs
+function barChart(sample)
+{
+    d3.json("samples.json").then((data) => {
+        let result = data.samples.filter(sampleResult => sampleResult.id == sample)[0];
+
+        let yticks = result.otu_ids.slice(0, 10).map(id => `OTU ${id}`);
+        let heights = result.sample_values.slice(0, 10);
+        let text = result.otu_labels.slice(0, 10);
+
+        let params = {
+            y: yticks.reverse(),
+            x: heights.reverse(),
+            text: text.reverse(),
+            type: "bar",
+            orientation: "h"
+        };
+
+        let layout = {
+            title: "Top 10 Belly Button Bacteria"
+        };
+
+        Plotly.newPlot("bar", [params], layout);
+    })
+}
